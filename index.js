da712cd97d7625e59457e579f97e3d3a646ba393
mac <mac@macs-MacBook-Pro.local>
2023-07-21 16:35:03 +0530

    initial Commit

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-let set = new Set();
-let weakset = new WeakSet();
+// Assignment Submitted in README.md
 
-let obj = { key: "value" };
-
-set.add(obj);
-weakset.add(obj);
-
-console.log(set.has(obj)); // true
-console.log(weakset.has(obj)); // true
-
-obj = null;
-
-console.log(set.has(obj)); // false
-console.log(weakset.has(obj)); // false
